d0b5754371b0d63e9b815550ab51d0c837cd2fbc
Vangeepuram Sreekesari <kesari.vangeepuram@gmail.com>
2022-01-30 02:23:34 +0530

    `UPDATE` main.go - Add CREATE method to faunaDB

--- main.go
+++ main.go
@@ -1,19 +1,88 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 	"time"
 
+	f "github.com/fauna/faunadb-go/v4/faunadb"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 type User struct {
-	ID          string    `json:"id,omitempty"`
-	Name        string    `json:"name"`
-	DOB         string    `json:"dob"`
-	Address     string    `json:"address"`
-	Description string    `json:"description"`
-	CreatedAt   time.Time `json:"createdAt,omitempty"`
+	ID          string    `json:"id,omitempty" fauna:"id"`
+	Name        string    `json:"name" fauna:"name"`
+	DOB         string    `json:"dob" fauna"dob"`
+	Address     string    `json:"address" fauna:"address"`
+	Description string    `json:"description" fauna:"description"`
+	CreatedAt   time.Time `json:"createdAt,omitempty" fauna:"createdAt"`
+}
+
+// Global variable to access admin-client to faunaDB
+var adminClient *f.FaunaClient
+
+func init() {
+
+	var err error = godotenv.Load()
+	if err != nil {
+		panic("[ERROR]: Unable to load <.env> file!")
+	}
+
+	// Get a new fauna client with access key
+	adminClient = f.NewFaunaClient(os.Getenv("FAUNADB_ADMIN_SECRET"))
+
+	// Create a persistent database node for the API
+	result, err := adminClient.Query(
+		f.CreateDatabase(f.Obj{"name": "golang-api"}))
+
+	handleError(result, err)
+
+	// Create a collection (table) inside the database
+	result, err = adminClient.Query(
+		f.CreateCollection(f.Obj{"name": "Users"}))
+
+	handleError(result, err)
+
+	// Create an index to access documents easily
+	result, err = adminClient.Query(
+		f.CreateIndex(
+			f.Obj{
+				"name":   "users_by_id",
+				"source": f.Collection("Users"),
+				"terms":  f.Arr{f.Obj{"field": f.Arr{"data", "id"}}},
+			},
+		))
+
+	handleError(result, err)
+
+}
+
+// Handles the error and prompts the result accordingly
+func handleError(result f.Value, err error) {
+	if err != nil {
+		fmt.Printf("[FAUNADB-WARN]: ")
+		fmt.Fprintln(os.Stderr, err)
+	} else {
+		fmt.Printf("[FAUNADB-DEBUG]: ")
+		fmt.Println(result)
+	}
+}
+
+// Fetch new ID for a document on call
+func newID() (id string, err error) {
+	result, err := adminClient.Query(f.NewId())
+	if err != nil {
+		return "", err
+	}
+
+	err = result.Get(&id)
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
 }
 
 // slice of <User>s - for temporary use only!
@@ -67,17 +136,50 @@ func getUserByID(ctx *gin.Context) {
 
 // createUsers responds to POST requests to API
 func createUsers(ctx *gin.Context) {
-	var newUser User
 
+	var newUser User
 	if err := ctx.BindJSON(&newUser); err != nil {
+		ctx.IndentedJSON(
+			http.StatusNotAcceptable,
+			gin.H{"message": "Invalid JSON!"},
+		)
 		return
 	}
 
-	// Adding/Updating the user's ctime
+	// Get new id for document
+	id, err := newID()
+	if err != nil {
+		ctx.IndentedJSON(
+			http.StatusInternalServerError,
+			gin.H{"message": "Unable to generate an id for the user!"},
+		)
+		return
+	}
+
+	// Adding the user's id & ctime
+	newUser.ID = id
 	newUser.CreatedAt = time.Now()
 
 	// Change the below line -> Upsert to DB
-	users = append(users, newUser)
+	users = append(users, newUser) // DEBUG
+	result, err := adminClient.Query(
+		f.Create(
+			f.Ref(f.Collection("Users"), id),
+			f.Obj{
+				"data": f.Obj{
+					"id":          newUser.ID,
+					"name":        newUser.Name,
+					"dob":         newUser.DOB,
+					"address":     newUser.Address,
+					"description": newUser.Description,
+					"createdAt":   newUser.CreatedAt,
+				},
+			},
+		))
+
+	handleError(result, err)
+
+	// Respond client
 	ctx.IndentedJSON(http.StatusCreated, newUser)
 }
 
